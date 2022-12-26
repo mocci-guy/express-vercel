d55475845034d3645f42295c1f23c119aaa10bd3
hamburger <tik.shitting@gmail.com>
2022-12-26 22:27:36 +0200

    supply+circulation

--- routes/circulationSupply.js
+++ routes/circulationSupply.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const tokenContract = require("../contractInfo.js");
+const addresses = require("../addresses.js");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  return res.status(200).json(123);
+  let frozenAmount = 0; // Amount of tokens that exist out of circulation
+  for (let i = 0; i < addresses.length; i++) {
+      const element = addresses[i];
+      const walletBalance = await tokenContract.methods.balanceOf(element).call();
+      frozenAmount += +(walletBalance);
+  }
+  const decimals = await tokenContract.methods.decimals().call();
+  const totalSupply = await tokenContract.methods.totalSupply().call();
+  const circulationSupply = totalSupply - frozenAmount;
+  const adjustedCirculationSupply = circulationSupply * 10 ** -decimals;
+  return res.status(200).json(adjustedCirculationSupply);
 });
 
 module.exports = router;
